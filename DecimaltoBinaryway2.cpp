a784d28b448c289d492b915112464356458bc88c
Monu <monugandhi5911@gmail.com>
2025-09-14 00:25:54 +0530

    Added binary conversion code using right shift

---
+++ DecimaltoBinaryway2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter decimal number: ";
+    cin >> num;
+
+    cout << "Binary representation: ";
+    for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        cout << bit;
+    }
+    cout << endl;
+
+    return 0;
+}
